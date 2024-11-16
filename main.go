50835ca91d16f7a5d5cc04fdc66cb03163746f80
Bhavikk Shah <bhavikk.shah33@gmail.com>
2024-11-16 04:07:36 -0600

    Adding updates

--- main.go
+++ main.go
@@ -186,7 +186,7 @@ func main() {
 	
 	//Option 4
 	//Database application running on Google Cloud Platform. 
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=34.122.174.83 sslmode=disable port=5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/seismic-elf-441907-h2:us-central1:mypostgres sslmode=disable port=5432"
 	
 
 	db, err := sql.Open("postgres", db_connection)
